3f5a7ebe86c1abf8236c72063437f725afb8a8fa
Herald-0f-l1fe <boriskovalkov003@gmail.com>
2026-04-14 16:04:02 +0300

    add pwm_dac.py and fix r2r_sin.py

--- get-dac/r2r_sin.py
+++ get-dac/r2r_sin.py
@@ -1,21 +1,18 @@
 import r2r_dac as r2r
-import signal_generator as sg
+import numpy as np
 import time
 
-amplitude = 3.2          
+amplitude = 1          
 signal_frequency = 10    
 sampling_frequency = 1000 
 
 dac_pins = [16, 20, 21, 25, 26, 17, 27, 22]
-max_v = 3.3
-
-import numpy as np
-import time
+max_v = 3.16
 
 def get_sin_wave_amplitude(freq, t):
-    # Формула: (sin(2 * pi * f * t) + 1) / 2
+    # Формула: (sin(2 * pi * f * t) + 1)
     sin_val = np.sin(2 * np.pi * freq * t)
-    return (sin_val + 1) / 2
+    return (sin_val + 1)
 
 def wait_for_sampling_period(sampling_frequency):
     time.sleep(1 / sampling_frequency)
@@ -32,15 +29,16 @@ try:
     while True:
         current_time = time.time() - start_time
         
-        norm_amp = sg.get_sin_wave_amplitude(signal_frequency, current_time)
+        norm_amp = get_sin_wave_amplitude(signal_frequency, current_time)
         
         target_voltage = norm_amp * amplitude
         dac.set_voltage(target_voltage)
         
-        sg.wait_for_sampling_period(sampling_frequency)
+        wait_for_sampling_period(sampling_frequency)
 
 except KeyboardInterrupt:
     print("\nГенерация остановлена пользователем")
 
 finally:
     dac.deinit()
+    
